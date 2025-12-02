c8fcc9012aab50aff38aede5625a7c35bdae2251
merosman91 <merghanigasimosman@gmail.com>
2025-12-02 14:27:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: './icon-192.png',
+            src: './icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
